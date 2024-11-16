49d83e96cc3c8b54d21e6e88c8189c1c3e7bba69
Alex Sogorb <164156602+asogorb@users.noreply.github.com>
2024-11-16 10:36:26 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import uuid
+from flask import Flask, request
+from flask_smorest import abort
+from db import stores, items
+
+
+app = Flask(__name__)
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        abort(404, message="Store not found")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        # Here you might also want to add the items in this store
+        # We'll do that later on in the course
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
